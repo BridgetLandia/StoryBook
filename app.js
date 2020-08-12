083a7b1ad0a6e5eb6d31780d37c29540a643961a
Brigitta <bridgetlandia@gmail.com>
2020-08-12 15:02:53 +0200

    Adding Story Model and add story page

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
 
 //Load config
@@ -16,6 +18,10 @@ require('./config/passport')(passport);
 connectDB();
 
 const app = express();
+
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //Logging
 if (process.env.NODE_ENV === 'development') {
@@ -31,7 +37,8 @@ app.use(
 	session({
 		secret: 'keyboard cat',
 		resave: false,
-		saveUninitialized: false
+		saveUninitialized: false,
+		store: new MongoStore({ mongooseConnection: mongoose.connection })
 	})
 );
 
@@ -46,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 5000;
 
