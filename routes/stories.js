923d57fc59a61a74126004d016631096c509b70e
Brigitta <bridgetlandia@gmail.com>
2020-08-12 17:22:25 +0200

    Adding Edit page, helpers, global var

--- routes/stories.js
+++ routes/stories.js
@@ -10,6 +10,39 @@ router.get('/add', ensureAuth, (req, res) => {
 	res.render('stories/add');
 });
 
+//@dessc  Show all stories
+//@route  GET /stories
+
+router.get('/', ensureAuth, async (req, res) => {
+	try {
+		const stories = await Story.find({ status: 'public' }).populate('user').sort({ createdAt: 'desc' }).lean();
+		res.render('stories/index', { stories });
+	} catch (err) {
+		console.error(err);
+		res.render('error/500');
+	}
+});
+
+//@dessc  Show edit page
+//@route  GET /stories/:id
+
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+	try {
+		const story = await Story.findOne({ _id: req.params.id }).lean();
+		if (!story) {
+			return res.render('error/404');
+		}
+		if (story.user != req.user.id) {
+			res.redirect('/stories');
+		} else {
+			res.render('stories/edit', { story });
+		}
+	} catch (err) {
+		console.error(err);
+		res.render('error/500');
+	}
+});
+
 //@dessc  Process add
 //@route  POST /stories
 
